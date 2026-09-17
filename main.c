69003606b2ac3892cfe0729a806327c99156f6dc
bluepupple <sseepark@sookmyung.ac.kr>
2026-09-17 16:50:39 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main(void){
- int a, b;
+ int a, b; 
 
-printf("input a number :");
-scanf("%d", &a);
+printf("input a number :"); 
+scanf("%d", &a); 
 
-printf("input a number :");
-scanf("%d", &b);
+printf("input a number :"); 
+scanf("%d", &b); 
 
-printf("Dividing result is %f\n", (float)a/b);
+printf("Dividing result is %f\n", (float)a/b); 
 
-return 0;
-}
+return 0; 
+} 
